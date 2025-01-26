b20f6a0a0bdabe210dbed2d35c41614ed8ddfad7
LavellM25 <Lavellmcgrone25@gmail.com>
2025-01-25 21:13:25 -0600

    Create Question5.java

---
+++ week0/Question5.java
@@ -0,0 +1,142 @@
+package week_0;
+
+// Date: 1-21-2025
+// Author: Lavell McGrone
+
+/**
+*
+ * Class setup checklist
+ *
+ * This isn't really a program, but read through the checklist, complete the items, and change
+ * the variables as you complete each task so the autograder knows you've done all the things.
+ *
+ * Any questions or problems with any of these things? Please get in touch!
+ *
+ * */
+
+
+ public class Question_5_Class_Setup {
+
+
+    /* (If using your own computer)
+    *   Installing software:  Install Java SDK, on your own computer
+    *
+    *   OR
+    *
+    *   If you''ll be using lab computers, make sure you know when the department is open and
+    *   plan when you'll be here to do your work.
+    *
+    *  TODO Set this boolean to true once you've completed this
+    */
+
+    public boolean haveInstalledJava_OR_MadePlanToUseLabComputers = true;
+
+
+
+    /*  (If using your own computer and you have a PC)
+    *   Installing software:  Install git on your own computer
+    *   https://git-scm.com/
+    *
+    *   Mac and Linux users: you probably already have git.
+    *   Type git --version at a terminal to verify.  If not, install from the link above.
+    *
+    *   TODO Set this boolean to true once you've completed this
+    */
+
+    public boolean haveInstalledOrConfirmedInstallOfGit = true;
+
+
+    /* Install IntelliJ *** Ultimate *** Edition on your own computer,
+    *  or verify you can use it on the college lab computers.
+    *  TODO Set these booleans to true once you've completed this
+    */
+
+    public boolean haveInstalledIntelliJ = true;
+    public boolean installedUltimateEdition_notCommunityEdition = false;
+
+
+
+    /*  Verify you can create and run a Hello World java program on your own computer,
+    *   or the college lab computers.
+    *
+    *   TODO Set this boolean to true once you've completed this
+    */
+
+    public boolean canRunHelloWorldProgram = true;
+
+
+
+    /*  Read the entire syllabus
+    *   Contact me ASAP if you have any question on the syllabus or class schedule.
+    *
+    *   TODO Set these booleans to true once you've completed this
+    */
+
+    public boolean haveReadEntireSyllabus = true;
+    public boolean contactedInstructorWithAnyQuestionsOnSyllabus = true;
+
+
+
+    /*  Plan block(s) of time per week that you will use to work on the class materials; labs and projects
+    *  This is a 6-credit class and typically students will need to spend 2x hours outside of class
+    *  You will need around 12 hours per week in addition to class time, to work on this class
+     *  If you are using our computers, make sure you know what the lab opening hours are.
+    *
+    *  TODO Set this boolean to true once you've completed this
+    */
+
+    public boolean havePlannedWhenToWorkOnClass = true;
+
+
+
+    /* Labs
+    *
+    * Make sure you have the labs set up. You should have downloaded the lab repository
+    * You should have your own GitHub account
+    * You should know how to commit your changes and push them to your own GitHub account
+    *
+    * Make sure you understand how to find each week's lab work
+    * Make sure you have the schedule for lab deadlines
+    *
+    * TODO Set these boolean to true once you've completed this
+    */
+
+    public boolean understandHowToDoLabs = true;
+    public boolean haveLabRepoAndGitHubSetUpCorrectly = true;
+
+
+
+    /*  Make sure you know when open lab/office hours are
+     *  TODO Set this boolean to true once you've completed this
+    */
+
+    public boolean knowWhenOpenLabIs = true;
+
+
+
+    /*  This one is important.
+    *  If you get stuck on something, or don't understand what's going on with something,
+    *  or git/GitHub or IntelliJ isn't working, you should ask for help ASAP.
+    *
+    *  Plan to email me ASAP, or ask in class/open lab if you have any questions on
+    *  anything to do with the class
+    *
+    *  Questions are encouraged! This is a tricky class, there's a lot of possible problems you
+    *  may run into, but I am here to help and I want you to succeed!
+    *
+    *  There's a strong correlation between number of questions asked and good grades
+    *  And please remember that I'm here to help you.
+    *
+    *  TODO Set this boolean to true once you've completed this
+    */
+
+    public boolean willAskForHelpIfNeeded = true;
+
+
+   public static void main(String[] args) {
+      System.out.println("Make sure you have everything set up for the class.\n" +
+              "Change the boolean variables from true to false as you complete the tasks.\n" +
+              "You don't need to write any other code for this questions.\n" +
+              "If you need any help, please ask! Thank you!");
+   }
+}
